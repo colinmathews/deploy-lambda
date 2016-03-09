6c02e904f2e680adab43f3939aaf0d11192c444c
Colin Mathews <colin.mathews@gmail.com>
2016-03-09 13:02:33 -0500

    Remove abstract class because it's causing problems

--- lib/tasks/task-base.ts
+++ lib/tasks/task-base.ts
@@ -3,8 +3,13 @@ import DeployConfig from '../models/deploy-config';
 import execute from '../util/execute';
 import path = require('path');
 
-abstract class TaskBase {
-  abstract run(config: DeployConfig): Promise<any>;
+// TODO: This should be an abstract class, but tsc isn't liking that
+class TaskBase {
+
+  // TODO: This should be an abstract method
+  run(config: DeployConfig): Promise<any> {
+    throw new Error('Subclasses of TaskBase must override this method');
+  }
 
   protected rootPath(): string {
     let root = path.resolve(__dirname, '../../../');
